9512ab6bac744a2a1c15d2f86643eee4a9fb51bd
bitsonymousm <bitsonymous@gmail.com>
2023-09-03 22:20:06 +0530

    d

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import App from './App';
 import Navbar from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Testimonials } from './components/Testimonials';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
+import Statistic from './components/Statistics';
+import { About } from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
+    <Hero/>
+    <HeadlineCards/>
+    <Statistic/>
+    <About/>
     <App />
     <Footer/>
   </React.StrictMode>
